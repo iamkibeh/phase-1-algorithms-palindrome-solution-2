b2b5e79153dd971587395ce440982fe38f3b8302
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-08-25 19:57:21 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length; i++) {
+    const j= word.length-1-i;
+    
+    return word[i]===word[j] 
+    }
+  return true
 }
 
 /* 
   Add your pseudocode here
+
+  loop through each character in word from the start abd the end
+  if character at the start is equals to character st the end, increment index at start and decrement index at last character and compare
+
+
 */
 
 /*
-  Add written explanation of your solution here
+  
+Add written explanation of your solution here
+
+first loop
+  r a c e c a r
+  0 1 2 3 4 5 6
+  i           j
+
+second loop
+  r a c e c a r
+  0 1 2 3 4 5 6
+    i       j
+
+  third loop
+  r a c e c a r
+  0 1 2 3 4 5 6
+      i   j
+
+  r a c e c a r
+  0 1 2 3 4 5 6
+        ij
+
+    return true
 */
 
 // You can run `node index.js` to view these console logs
